a5416a3ae654474b6e136ba6eee9c98118a77e87
renfushuai <renfushuai@xdf.cn>
2020-06-08 09:40:18 +0800

    添加console

---
+++ Blog.Console/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Blog.Console
+{
+    class TestModel
+    {
+        string Name { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            List<TestModel> nums_1 = new List<TestModel>();
+            List<TestModel> nums_2 = new List<TestModel>();
+            System.Console.WriteLine("第一种方法开始:");
+            TestModel model = null;
+            for (int i = 0; i < 1000000; i++)
+            {
+                nums_1.Add(model);
+            }
+            sw.Stop();
+            System.Console.WriteLine("程序结束，用时:" + (sw.ElapsedMilliseconds));
+            sw.Restart();
+            System.Console.WriteLine("第二种方法开始:");
+            for (int i = 0; i < 1000000; i++)
+            {
+                TestModel t = new TestModel();
+                nums_2.Add(t);
+            }
+            sw.Stop();
+            //System.out.println(nums_2);
+            System.Console.WriteLine("程序结束，用时:" + (sw.ElapsedMilliseconds));
+        }
+    }
+}
---
+++ BlogAdmin/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogAdmin.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        [Route("Index")]
+        [AllowAnonymous]
+        public string Index(string str)
+        {
+            int stmp = 10 / str.ObjToInt();
+            var bys = Convert.FromBase64String(str);
+            var s = BitConverter.ToString(bys);
+            return s;
+        }
+    }
+}
