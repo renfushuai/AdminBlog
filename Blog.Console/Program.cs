d5a3f224c201438b6d8c219bbb7048c5131079e8
renfushuai <renfushuai@xdf.cn>
2020-07-17 13:40:39 +0800

    验证线程可见性

--- Blog.Console/Program.cs
+++ Blog.Console/Program.cs
@@ -1,40 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+using System.Threading;
 
 namespace Blog.Console
 {
-    class TestModel
-    {
-        string Name { get; set; }
-    }
     class Program
     {
+        static bool flag = false;
         static void Main(string[] args)
         {
+            new Thread(() =>
+            {
+                System.Console.WriteLine("start....");
+                System.Console.WriteLine("子线程ID:" + Thread.CurrentThread.ManagedThreadId.ToString());
+                Thread.Sleep(1000);
+                flag = true;
+                System.Console.WriteLine("end....flag="+flag);
+            }).Start();
 
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            List<TestModel> nums_1 = new List<TestModel>();
-            List<TestModel> nums_2 = new List<TestModel>();
-            System.Console.WriteLine("第一种方法开始:");
-            TestModel model = null;
-            for (int i = 0; i < 1000000; i++)
+            while (true)
             {
-                nums_1.Add(model);
+                if (flag)
+                {
+                    System.Console.WriteLine("主线程ID:" + Thread.CurrentThread.ManagedThreadId.ToString());
+                }
             }
-            sw.Stop();
-            System.Console.WriteLine("程序结束，用时:" + (sw.ElapsedMilliseconds));
-            sw.Restart();
-            System.Console.WriteLine("第二种方法开始:");
-            for (int i = 0; i < 1000000; i++)
-            {
-                TestModel t = new TestModel();
-                nums_2.Add(t);
-            }
-            sw.Stop();
-            //System.out.println(nums_2);
-            System.Console.WriteLine("程序结束，用时:" + (sw.ElapsedMilliseconds));
+
+
         }
+
     }
 }
